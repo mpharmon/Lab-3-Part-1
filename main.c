4ce39ac019d82442332ef1546f239a1fdff17abf
Mike <Mike@auth-10-3-1-222.wifi.mphnetinc.net>
2016-03-24 19:00:32 -0700

    Final Draft

--- main.c
+++ main.c
@@ -30,8 +30,8 @@ void main(void){
     sprintf(string,"Voltage: %4.2fVdc",(ADC1BUF0/1023.0*3.3));
     LCD_PrintString(string);
     LCD_MoveCursor(1,1);
-    PWM_SetBackward();
-    PWM_SetOC1DutyCycle(25);
-    PWM_SetOC2DutyCycle(75);
+    PWM_SetForward();
+    PWM_SetOC1DutyCycle(75);
+    PWM_SetOC2DutyCycle(25);
   };
 }
